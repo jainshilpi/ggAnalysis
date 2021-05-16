ed5920ee872e5e819e2c64d9b22f520804289986
Mohammad <abrar.discloses@gmail.com>
2021-05-16 10:42:45 -0500

    Added more SS variables

--- ggNtuplizer/test/batch/onMC_UL/csub_xrootd.sh
+++ ggNtuplizer/test/batch/onMC_UL/csub_xrootd.sh
@@ -19,7 +19,8 @@ cmsswDir=/local/cms/user/wadud/aNTGCmet/CMSSW_10_6_24/src/ggAnalysis/ggNtuplizer
 pset="/local/cms/user/wadud/aNTGCmet/CMSSW_10_6_24/src/ggAnalysis/ggNtuplizer/test/run_condor_mc2017_106X.py"
 writeDir=/local/cms/user/wadud/aNTGCmet/ntuples/UL2017_v1/
 workDir=/local/cms/user/wadud/aNTGCmet/CMSSW_10_6_24/src/ggAnalysis/ggNtuplizer/test/batch/onMC_UL/jobs_v1/
-splitfiles=10
+splitfiles=5
+nRetries=5
 
 x509file=/tmp/x509up_u926763
 
@@ -97,6 +98,9 @@ function submitSubJob(){
 	sed -i 's|#jobList|'${fileListPath}'|g' ${runScript}
 	sed -i 's|#outFile|'${outFile}'|g' ${runScript}
 	sed -i 's|#X509FILE|'${x509copy}'|g' ${runScript}
+	sed -i 's|#nRetries|'${nRetries}'|g' ${runScript}
+
+	
 	chmod +x ${runScript}
 
 	### prepare condor script ###
--- ggNtuplizer/test/batch/onMC_UL/runOnMCxrdTemplate.sh
+++ ggNtuplizer/test/batch/onMC_UL/runOnMCxrdTemplate.sh
@@ -13,12 +13,11 @@ eval `scramv1 runtime -sh`
 
 export X509_USER_PROXY=#X509FILE
 voms-proxy-info -all
-voms-proxy-info -all -file ${X509_USER_PROXY}
-
 if [ ! -f "$X509_USER_PROXY" ]; then
     echo "ERROR! x509 proxy wasn't found!!"
     exit 113
 fi
+voms-proxy-info -all -file ${X509_USER_PROXY}
 
 echo -e "\n\n"
 
@@ -26,7 +25,19 @@ git show --format="%h %T %cn %cI %s" --no-patch
 
 echo -e "\n\n"
 
-cmsRun --jobreport=#jobDir/jobReport/#jobName.xml #pset InputFileList=#jobList outputFile=#outFile maxEvents=-1
+n=0
+until [ "$n" -ge #nRetries ]
+do
+	echo -e "Try number "$n
+
+	date
+
+	cmsRun --jobreport=#jobDir/jobReport/#jobName.xml #pset InputFileList=#jobList outputFile=#outFile maxEvents=-1 && break
+
+	n=$((n+1)) 
+
+	sleep 10
+done
 
 echo -e "Complete!"
 
